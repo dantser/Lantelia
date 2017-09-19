def455e221290e89326e9fb5773467ac3c7efad8
dantser <dantser@gmail.com>
2017-09-19 15:36:47 +0300

    fix

--- app/components/whereToBuy/whereToBuy.js
+++ app/components/whereToBuy/whereToBuy.js
@@ -1,13 +1,17 @@
+/* eslint-disable */
 import Swiper from 'swiper';
 // import $ from 'jquery';
+import 'selectric';
 
 export default () => {
-  /* eslint-disable */
+
   const mainSlider = new Swiper('.address-container', {
     wrapperClass: 'address-wrapper',
     slideClass: 'address-card',
     slidesPerView: 'auto'
-
   });
+
+  $('select').selectric();
+
   /* eslint-ensable */
 }
