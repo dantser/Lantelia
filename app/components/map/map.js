aeec0f8e5f30156bbab6153ae5632e191fc5eba0
dantser <dantser@gmail.com>
2017-10-06 21:09:26 +0300

    map fix

--- app/components/map/map.js
+++ app/components/map/map.js
@@ -28,29 +28,29 @@ export default () => {
   });
 
   var markers = [];
-
-  var tsetnoyMsk = new LeafIcon({iconUrl: 'assets/images/pin.svg'});
-  markers["marker1"] = L.marker([55.7788008, 37.5821708], {icon: tsetnoyMsk}, {title: marker1}).addTo(mymap)
+  var iconInactive = new LeafIcon({iconUrl: 'assets/images/pin 2.svg'});
+  var iconActive = new LeafIcon({iconUrl: 'assets/images/pin.svg'});
+  markers["marker1"] = L.marker([55.7788008, 37.5821708], {icon: iconInactive}, {title: marker1}).addTo(mymap)
                       .bindPopup(L.popup({maxWidth:201})
                       .setContent("<h3>Цветной Москва</h3> <p>г. Москва, Ленинградский проспект, д. 2, цокольный этаж, оф. №1</p><h3>Контакты</h3> <p>Тел.: 79269464613<br>Эл. почта: msk.bel@cdek.ru</p> <p>Пн-Пт – с 10:00 до 20:00<br>Сб, Вс – с 10:00 до 16:00</p>")).on('click', clickZoom);
 
-  var moulinRouge = new LeafIcon({iconUrl: 'assets/images/pin.svg'});
-  markers["marker2"] = L.marker([60.010829, 30.402103], {icon: moulinRouge}, {title: marker2}).addTo(mymap)
+
+  markers["marker2"] = L.marker([60.010829, 30.402103], {icon: iconInactive}, {title: marker2}).addTo(mymap)
                       .bindPopup(L.popup({maxWidth:201})
                       .setContent("<h3>Мулен Руж</h3> <p>г. Санкт-Петербург, просп. Науки, 23а</p><h3>Контакты</h3> <p>Тел.: 79269464613<br>Эл. почта: msk.bel@cdek.ru</p> <p>Пн-Пт – с 10:00 до 20:00<br>Сб, Вс – с 10:00 до 16:00</p>")).on('click', clickZoom);
 
-  var petrPassage = new LeafIcon({iconUrl: 'assets/images/pin.svg'});
-  markers["marker3"] = L.marker([57.817232, 28.314583], {icon: petrPassage}, {title: marker3}).addTo(mymap)
+
+  markers["marker3"] = L.marker([57.817232, 28.314583], {icon: iconInactive}, {title: marker3}).addTo(mymap)
                       .bindPopup(L.popup({maxWidth:201})
                       .setContent("<h3>Петровский Пассаж</h3> <p>г. Псков, Рижский пр-кт, д.16</p><h3>Контакты</h3> <p>Тел.: 79269464613<br>Эл. почта: msk.bel@cdek.ru</p> <p>Пн-Пт – с 10:00 до 20:00<br>Сб, Вс – с 10:00 до 16:00</p>")).on('click', clickZoom);
 
-  var iconFour = new LeafIcon({iconUrl: 'assets/images/pin.svg'});
-  markers["marker4"] = L.marker([57.817232, 28.314583], {icon: iconFour}, {title: marker4}).addTo(mymap)
+
+  markers["marker4"] = L.marker([57.234133, 28.314583], {icon: iconInactive}, {title: marker4}).addTo(mymap)
                       .bindPopup(L.popup({maxWidth:201})
                       .setContent("<h3>Петровский Пассаж</h3> <p>г. Псков, Рижский пр-кт, д.16</p><h3>Контакты</h3> <p>Тел.: 79269464613<br>Эл. почта: msk.bel@cdek.ru</p> <p>Пн-Пт – с 10:00 до 20:00<br>Сб, Вс – с 10:00 до 16:00</p>")).on('click', clickZoom);
 
-  var iconFive = new LeafIcon({iconUrl: 'assets/images/pin.svg'});
-  markers["marker5"] = L.marker([57.817232, 28.314583], {icon: iconFive}, {title: marker5}).addTo(mymap)
+
+  markers["marker5"] = L.marker([57.234123, 28.314583], {icon: iconInactive}, {title: marker5}).addTo(mymap)
                       .bindPopup(L.popup({maxWidth:201})
                       .setContent("<h3>Петровский Пассаж</h3> <p>г. Псков, Рижский пр-кт, д.16</p><h3>Контакты</h3> <p>Тел.: 79269464613<br>Эл. почта: msk.bel@cdek.ru</p> <p>Пн-Пт – с 10:00 до 20:00<br>Сб, Вс – с 10:00 до 16:00</p>")).on('click', clickZoom);
 
@@ -58,10 +58,18 @@ export default () => {
   // L.marker([55.7788008, 37.5821708], {icon: greenIcon}).addTo(mymap).bindPopup(L.popup({maxWidth:201}).setContent("<h3>Цветной Москва</h3> <p>г. Москва, Ленинградский проспект, д. 2, цокольный этаж, оф. №1</p><h3>Контакты</h3> <p>Тел.: 79269464613<br>Эл. почта: msk.bel@cdek.ru</p> <p>Пн-Пт – с 10:00 до 20:00<br>Сб, Вс – с 10:00 до 16:00</p>"));
   // var greenIcon = new LeafIcon({iconUrl: 'assets/images/pin.svg'});
   // L.marker([55.7788008, 37.5821708], {icon: greenIcon}).addTo(mymap).bindPopup(L.popup({maxWidth:201}).setContent("<h3>Цветной Москва</h3> <p>г. Москва, Ленинградский проспект, д. 2, цокольный этаж, оф. №1</p><h3>Контакты</h3> <p>Тел.: 79269464613<br>Эл. почта: msk.bel@cdek.ru</p> <p>Пн-Пт – с 10:00 до 20:00<br>Сб, Вс – с 10:00 до 16:00</p>"));
+  const markerCl = L.markerClusterGroup([55.7788008, 37.5821708]).addTo(mymap);
 
+  mymap.addLayer(markerCl);
 
   function focusOn(marker) {
+    var pos = markers[marker].getLatLng();
+    mymap.setView(pos, 18);
     markers[marker].openPopup();
+    for (var i in markers) {
+      markers[i].setIcon(iconInactive);
+    }
+    markers[marker].setIcon(iconActive);
   };
 
   L.control.zoom({
@@ -74,8 +82,7 @@ export default () => {
 
   $('.address-card').click(function(){
     var currMarker = $(this).find('.address-card__wrapper').attr('id');
-    console.log(currMarker);
-    markerFunction(currMarker);
+    focusOn(currMarker);
   });
 
   // mymap.on('popupopen', function(centerMarker) {
@@ -87,9 +94,9 @@ export default () => {
   function markerFunction(id) {
     for (var i in markers) {
       var markerID = markers[i].options.title;
-      // var position = markers[i].getLatLng();
+      var position = markers[i].getLatLng();
       if (markerID == id) {
-      	// map.setView(position, 20);
+      	mymap.setView(position, 20);
         markers[i].openPopup();
       };
     }
